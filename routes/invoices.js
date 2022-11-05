a38bc7b4bc608d311605059e3e26ccc0228cb0f2
sebastyijan-fi <westinni@arcada.fi>
2022-11-05 10:30:40 +0200

    routes mockup

---
+++ routes/invoices.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const Invoice = require('../models/invoice')
+
+//POST invoice
+router.post('/', async (req, res) => {
+    
+   
+    });
+
+    
+module.exports = router
