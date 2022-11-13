cf65888c319b4e381ca13a111ad39da794fa4213
sebastyijan-fi <westinni@arcada.fi>
2022-11-13 09:38:40 +0200

    comments

--- routes/invoices.js
+++ routes/invoices.js
@@ -62,19 +62,8 @@ var data = {
     "bottom-notice": "Due date 15 days",
     // Settings to customize your invoice
     "settings": {
-        "currency": "EUR", // See documentation 'Locales and Currency' for more info. Leave empty for no currency.
-        // "locale": "nl-NL", // Defaults to en-US, used for number formatting (See documentation 'Locales and Currency')
-        // "tax-notation": "gst", // Defaults to 'vat'
-        // "margin-top": 25, // Defaults to '25'
-        // "margin-right": 25, // Defaults to '25'
-        // "margin-left": 25, // Defaults to '25'
-        // "margin-bottom": 25, // Defaults to '25'
-        // "format": "A4", // Defaults to A4, options: A3, A4, A5, Legal, Letter, Tabloid
-        // "height": "1000px", // allowed units: mm, cm, in, px
-        // "width": "500px", // allowed units: mm, cm, in, px
-        // "orientation": "landscape", // portrait or landscape, defaults to portrait
-    },
-    // Translate your invoice to your preferred language
+        "currency": "EUR"
+    }
     
 };
 
