a7cee645096f45fa678e9228bea4d820672888e2
sebastyijan-fi <westinni@arcada.fi>
2022-11-13 10:14:51 +0200

    email works. pdf attached works

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const invoice = require('../models/invoice')
-
-
+const nodemailer = require("nodemailer");
 var easyinvoice = require('easyinvoice');
 var fs = require('fs');
+
+//data for pdf
 var data = {
    
     "images": {
@@ -67,16 +68,57 @@ var data = {
     
 };
 
-//Create your invoice! Easy!
-easyinvoice.createInvoice(data, function (result) {
-    //The response will contain a base64 encoded PDF file
-    console.log('PDF base64 string: ', result.pdf);
-});
+
 
 easyinvoice.createInvoice(data, function (result) {
   
     fs.writeFileSync("invoice.pdf", result.pdf, 'base64');
 });
+
+
+// Email
+async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: "bar@example.com, baz@example.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+      attachments: [{
+      filename: 'invoice.pdf',
+      path: 'invoice.pdf',
+      contentType: 'application/pdf'
+    }]
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
+  
+  main().catch(console.error);
+
+
+
 
 router.post('/', async (req, res) => {
     
