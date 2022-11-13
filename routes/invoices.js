161f32fcae32120c33bea83f828ecda80590ef7d
sebastyijan-fi <westinni@arcada.fi>
2022-11-13 09:35:31 +0200

    create pdf - works

--- routes/invoices.js
+++ routes/invoices.js
@@ -5,7 +5,84 @@ const invoice = require('../models/invoice')
 
 var easyinvoice = require('easyinvoice');
 var fs = require('fs');
-var data = {};
+var data = {
+   
+    "images": {
+        // The logo on top of your invoice
+        "logo": ""
+    },
+    // Your own data
+    "sender": {
+        "company": "Sample Corp",
+        "address": "Sample Street 123",
+        "zip": "1234 AB",
+        "city": "Sampletown",
+        "country": "Samplecountry"
+        
+    },
+    // Your recipient
+    "client": {
+        "company": "Client Corp",
+        "address": "Clientstreet 456",
+        "zip": "4567 CD",
+        "city": "Clientcity",
+        "country": "Clientcountry"
+        
+    },
+    "information": {
+        // Invoice number
+        "number": "2021.0001",
+        // Invoice data
+        "date": "12-12-2021",
+        // Invoice due date
+        "due-date": "31-12-2021"
+    },
+    
+    "products": [
+        {
+            "quantity": 2,
+            "description": "Product 1",
+            "tax-rate": 6,
+            "price": 33.87
+        },
+        {
+            "quantity": 4.1,
+            "description": "Product 2",
+            "tax-rate": 6,
+            "price": 12.34
+        },
+        {
+            "quantity": 4.5678,
+            "description": "Product 3",
+            "tax-rate": 21,
+            "price": 6324.453456
+        }
+    ],
+    // The message you would like to display on the bottom of your invoice
+    "bottom-notice": "Due date 15 days",
+    // Settings to customize your invoice
+    "settings": {
+        "currency": "EUR", // See documentation 'Locales and Currency' for more info. Leave empty for no currency.
+        // "locale": "nl-NL", // Defaults to en-US, used for number formatting (See documentation 'Locales and Currency')
+        // "tax-notation": "gst", // Defaults to 'vat'
+        // "margin-top": 25, // Defaults to '25'
+        // "margin-right": 25, // Defaults to '25'
+        // "margin-left": 25, // Defaults to '25'
+        // "margin-bottom": 25, // Defaults to '25'
+        // "format": "A4", // Defaults to A4, options: A3, A4, A5, Legal, Letter, Tabloid
+        // "height": "1000px", // allowed units: mm, cm, in, px
+        // "width": "500px", // allowed units: mm, cm, in, px
+        // "orientation": "landscape", // portrait or landscape, defaults to portrait
+    },
+    // Translate your invoice to your preferred language
+    
+};
+
+//Create your invoice! Easy!
+easyinvoice.createInvoice(data, function (result) {
+    //The response will contain a base64 encoded PDF file
+    console.log('PDF base64 string: ', result.pdf);
+});
 
 easyinvoice.createInvoice(data, function (result) {
   
